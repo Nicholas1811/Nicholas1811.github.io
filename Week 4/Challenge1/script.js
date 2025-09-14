83645829b83aa6a364c487a85083a8bdb951ab91
Nicholas <nicholassdl660@gmail.com>
2025-09-14 11:21:10 +0800

    new stuff

---
+++ Week 4/Challenge1/script.js
@@ -0,0 +1,7 @@
+function calculate() {
+    var number1 = document.getElementById("number1").value;
+    var number2 = document.getElementById("number2").value;
+    document.getElementById("result").innerHTML = Number(number1)+ Number(number2);
+    // YOUR CODE GOES HERE
+    
+}
